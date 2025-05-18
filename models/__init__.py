c4fec82e99e8bd3f0e59565c902ec28b8c990bdb
mozkkkk <ax347689643@163.com>
2025-05-18 22:51:37 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .p2pnet import build as b1
-from .p2pnet2 import build as b2
-
-# build the P2PNet model
-# set training to 'True' during training
-def build_model(args, adapter=False, training=False):
-    if adapter:
-        return b2(args, training)
-    return b1(args, training)
